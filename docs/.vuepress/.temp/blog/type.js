c1d795da59959579555a427881bbd2fc5f479896
So <so@imeikan.com>
2025-05-11 11:31:42 +0800

    wip

--- docs/.vuepress/.temp/blog/type.js
+++ docs/.vuepress/.temp/blog/type.js
@@ -1,4 +1,4 @@
-export const typesMap = JSON.parse("{\"article\":{\"/\":{\"path\":\"/article/\",\"indexes\":[0]}},\"timeline\":{\"/\":{\"path\":\"/timeline/\",\"indexes\":[0]}}}");
+export const typesMap = JSON.parse("{\"article\":{\"/\":{\"path\":\"/article/\",\"indexes\":[1,0]}},\"timeline\":{\"/\":{\"path\":\"/timeline/\",\"indexes\":[1,0]}}}");
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept();
